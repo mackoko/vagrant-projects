c7c7497938dbd0997cc9c9ac4806292a3c1c2969
mackoko <maciekrz12@gmail.com>
2017-04-22 22:32:00 +0200

    bootstrap with java

--- vagrant-puppet/puppet/manifests/default.pp
+++ vagrant-puppet/puppet/manifests/default.pp
@@ -1,4 +1,14 @@
-include mysql
+#include mysql
+include stdlib
+include apt
+
+class { 'elasticsearch':
+  java_install => true,
+  manage_repo  => true,
+  repo_version => '5.x',
+}
+
+elasticsearch::instance { 'es-01': }
 
 
 # https://serverfault.com/questions/197909/puppet-class-inheritance-confusion
--- vagrant-shell/bootstrap.sh
+++ vagrant-shell/bootstrap.sh
@@ -1,3 +1,14 @@
-# install openjdk-8
-sudo apt-get purge openjdk*
-sudo apt-get -y install openjdk-8-jdk
+# install java
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export JRE_HOME=/usr/lib/jvm/java-8-oracle/jre
+
+sudo apt-get -y install oracle-java8-installer
+
+# install elastic
+
+# install kibana
