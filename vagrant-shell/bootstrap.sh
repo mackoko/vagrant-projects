648779e3583dea247bc0d0a4602d6b4eedce4dbf
mackoko <maciekrz12@gmail.com>
2017-04-23 14:43:32 +0200

    works works ?

--- vagrant-shell/bootstrap.sh
+++ vagrant-shell/bootstrap.sh
@@ -14,11 +14,13 @@ sudo apt-get -y install oracle-java8-installer
 #sudo apt-get update
 #sudo apt-get -y install elasticsearch
 sudo apt-get update
-sudo wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.1/elasticsearch-2.3.1.deb
-sudo dpkg -i elasticsearch-2.3.1.deb
+# sudo wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.1/elasticsearch-2.3.1.deb
+# sudo dpkg -i elasticsearch-2.3.1.deb
+sudo wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.1.deb
+sudo dpkg -i elasticsearch-5.3.1.deb
 
 #elastic on localhost
-sudo sed -i -e "s/\(network.host: \).*/\1localhost/" /etc/elasticsearch/elasticsearch.yml
+sudo sed -i -e "s/\(network.host: \).*/\10.0.0.0/" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i "s/#network.host/network.host/g" /etc/elasticsearch/elasticsearch.yml
 
 sudo sed -i 's/#START_DAEMON/START_DAEMON/' /etc/default/elasticsearch
@@ -32,11 +34,19 @@ systemctl status elasticsearch
 # https://www.digitalocean.com/community/tutorials/how-to-install-elasticsearch-logstash-and-kibana-elk-stack-on-ubuntu-14-04  - change file
 
 # install kibana
-echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
-sudo apt-get update
-sudo apt-get -y install kibana
+#echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
+#sudo apt-get update
+#sudo apt-get -y install kibana
+wget https://artifacts.elastic.co/downloads/kibana/kibana-5.3.1-amd64.deb
+sudo dpkg -i kibana-5.3.1-amd64.deb
 
 # kibana on localhost
-sudo sed -i "s/# elasticsearch.url/elasticsearch.url/g" /opt/kibana/config/kibana.yml
-sudo service kibana restart
+sudo sed -i "s/#elasticsearch.url/elasticsearch.url/g" /etc/kibana/kibana.yml
+
+sudo sed -i -e "s/\(server.host: \).*/\1\"0.0.0.0\"/" /etc/kibana/kibana.yml
+sudo sed -i "s/#server.host/server.host/g" /etc/kibana/kibana.yml
+
+sudo systemctl restart kibana
+systemctl status kibana
+# sudo service kibana restart
 # take a look https://github.com/stopsopa/elastic/blob/master/Vagrantfile
